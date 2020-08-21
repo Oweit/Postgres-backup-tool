7acbc2abfa32c2dca9022aacedb36d46118dd580
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2020-08-21 17:32:46 +0200

    add backupfolder and make generated file executable

--- init.sh
+++ init.sh
@@ -1,7 +1,14 @@
 #! /bin/sh
 
+mkdir backup
+
+echo "Enter name of postgres database that you want to backup"
+
 read db
 
 echo "#! /bin/sh
 
-pg_dump $(db) > backup/"$(date +'%Y-%m-%d')".bak" > backup.sh
+pg_dump $db > backup/"$(date +'%Y-%m-%d')".bak" > backup.sh
+
+chmod +x backup.sh
+
